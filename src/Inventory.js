93cbee4cd6e3a3e6b9102802c7156369a351d406
Meysam Salehi <m.salehi@tamitco.ir>
2022-05-15 16:57:05 +0430

    second commit

--- src/Inventory.js
+++ src/Inventory.js
@@ -132,65 +132,118 @@ const Inventory = () => {
           </div>
           <div>
             <ul>
-              <li className="py-4 hover:bg-stone-100 w-full border-b border-stone-100 ">
-                <a href="#" className="mx-4">
-                  لینک شماره یک
-                </a>
+              <li className=" px-4 py-4 hover:bg-stone-100 w-full border-b border-stone-100 flex flex-row justify-start gap-x-3">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+                  />
+                </svg>
+
+                <a href="#">صفحه اصلی </a>
               </li>
-              <li className="py-4 hover:bg-stone-100 w-full border-b border-stone-100 ">
-                <a href="#" className="mx-4">
-                  لینک شماره یک
-                </a>
-              </li>
-              <li className="py-4 hover:bg-stone-100 w-full border-b border-stone-100 ">
-                <a href="#" className="mx-4">
-                  لینک شماره یک
-                </a>
+              <li className=" px-4 py-4 hover:bg-stone-100 w-full border-b border-stone-100 flex flex-row justify-start gap-x-3">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  stroke-width="2"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"
+                  />
+                </svg>
+
+                <a href="#">لینک شماره یک</a>
               </li>
             </ul>
           </div>
         </div>
-        <div className="basis-8/12 max-h-10 border border-slate-200 rounded-lg flex justify-between items-center p-4">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="h-6 w-6 basis-1/12 flex justify-start"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            stroke-width="2"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z"
-            />
-          </svg>
-          <span className="text-xs text-orange-500 basis-8/12 flex justify-start">
-            برای افزایش امنیت حساب کاربری خود و جلوگیری از سوءاستفاده، لطفا هویت
-            خود را تایید کنید
-          </span>
 
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6 basis-2/12"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            strokeWidth={2}
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M15 19l-7-7 7-7"
-            />
-          </svg>
+        <div className="basis-8/12 ">
+          <div className="max-h-10 border border-slate-200 rounded-lg flex justify-between items-center p-4">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-6 w-6 basis-1/12 flex justify-start"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              stroke-width="2"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z"
+              />
+            </svg>
+            <span className="text-xs text-orange-500 basis-8/12 flex justify-start">
+              برای افزایش امنیت حساب کاربری خود و جلوگیری از سوءاستفاده، لطفا
+              هویت خود را تایید کنید
+            </span>
+
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6 basis-2/12"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth={2}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M15 19l-7-7 7-7"
+              />
+            </svg>
+          </div>
+          <div className="max-h-10 border border-slate-200 rounded-lg flex justify-between items-center p-4">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-6 w-6 basis-1/12 flex justify-start"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              stroke-width="2"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z"
+              />
+            </svg>
+            <span className="text-xs text-orange-500 basis-8/12 flex justify-start">
+              برای افزایش امنیت حساب کاربری خود و جلوگیری از سوءاستفاده، لطفا
+              هویت خود را تایید کنید
+            </span>
+
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6 basis-2/12"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth={2}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M15 19l-7-7 7-7"
+              />
+            </svg>
+          </div>
         </div>
-      </div>
-
-      <div className="flex flex-row justify-center items-center w-80	 gap-x-80">
-        <div>logo</div>
-        <div>logo</div>
-        <div>logo</div>
       </div>
     </div>
   );
